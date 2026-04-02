ce6594655b9c0cc475f6ae68eaf073844335323a
Hoshang Warfade <166973278+Hoshang-Warfade@users.noreply.github.com>
2026-04-02 10:40:33 +0530

    improved search layout and keep the search term on textbox

--- blogs/views.py
+++ blogs/views.py
@@ -35,8 +35,8 @@ def blog(request, slug):
 def search(request):
    keyword=request.GET.get('keyword')
    blogs=Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword)|Q(blog_body__icontains=keyword), status='Published')
-   print(keyword)
    context={
       'blogs': blogs,
+      'keyword': keyword
    }
    return render(request, 'search.html',context)
